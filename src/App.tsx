790b205b4a5d9fba8046282e78693d547aec03ee
otis <otislambert@gmail.com>
2023-06-09 16:30:22 -0700

    feat(app): created page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 function App() {
 	return (
 		<>
-			<h2>Bin 2 Dec Converter</h2>
+			<header className='header'>
+				<h1>Bin 2 Dec Converter</h1>
+			</header>
+			<main>
+				<div className='centered'>
+					<div className='input'>
+						<input type='text' />
+						<button>Convert!</button>
+					</div>
+					<div className='results'>
+						<p>Results go here...</p>
+					</div>
+				</div>
+			</main>
 		</>
 	);
 }
