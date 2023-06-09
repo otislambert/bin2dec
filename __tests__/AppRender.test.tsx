2bf8af5c3683b15d2a1a376e942aca85fa9dca7b
otis <otislambert@gmail.com>
2023-06-09 16:57:17 -0700

    refactor(test): Improve rendering tests and add placeholder for binary input

--- __tests__/AppRender.test.tsx
+++ __tests__/AppRender.test.tsx
@@ -6,7 +6,20 @@ import React from 'react';
 describe('App Rendering', () => {
 	it('renders a title', () => {
 		render(<App />);
-		const title = screen.findByText('Bin 2 Dec Converter');
+		const title = screen.getByText('Bin 2 Dec Converter');
 		expect(title).toBeDefined();
+		expect(title.nodeName).toBe('H1');
+	});
+	it('renders an input', () => {
+		render(<App />);
+		const input = screen.getByPlaceholderText('bin');
+		expect(input).toBeDefined();
+		expect(input.nodeName).toBe('INPUT');
+	});
+	it('renders a convert button', () => {
+		render(<App />);
+		const button = screen.getByText('Convert!');
+		expect(button).toBeDefined();
+		expect(button.nodeName).toBe('BUTTON');
 	});
 });
--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
 			<main>
 				<div className='centered'>
 					<div className='input'>
-						<input type='text' />
+						<input type='text' placeholder='bin' />
 						<button>Convert!</button>
 					</div>
 					<div className='results'>
